0a412716cfb4a94935ba473519ce44d5d9496bd9
Cody Condon <codyrcondon@gmail.com>
2025-01-21 16:03:39 -0500

    Initial Commit

---
+++ TrichJournal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrichJournal
+//
+//  Created by Cody Condon on 2025-01-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TrichJournalTests/TrichJournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrichJournalTests.swift
+//  TrichJournalTests
+//
+//  Created by Cody Condon on 2025-01-21.
+//
+
+import Testing
+@testable import TrichJournal
+
+struct TrichJournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
